9acadc9ea962a0f30f8fcf7adfbd9b6db302dce8
Robin <robinhallct@gmail.com>
2025-11-18 22:45:19 -0600

    Import classes to driver

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from profile import Profile
+from study_session import StudySession
+from invite_logic import InviteLogic
+from auto_cancel import AutoCancelJob
+from flashcards import FlashcardGenerator
 
+def main():
+    Alex = Profile("Alex", "CIS", "Math", )
